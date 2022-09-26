99b487c27ce41158370995771b2a0f9dd8db0082
lily <a0976198586@gmail.com>
2022-09-26 17:47:07 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -60,7 +60,7 @@ function renderData(){
   list.innerHTML = str;
 }
 renderData();
-toBeCompleteNum();
+toBeCompletedNum();
 
 // 篩選器
 const category = document.querySelector('.category');
@@ -113,12 +113,12 @@ category.addEventListener('click', function(e){
 
 // 篩選底線
 $('.category>li').click(function(){
-  $('li').removeClass('bb-1');
+  $('li').removeClass("bb-1");
   $(this).addClass('bb-1');
 });
 
 function categoryLiBottom(){
-  $('.category>li').removeClass('bb-1');
+  $('.category>li').removeClass("bb-1");
   $('.category>.all').addClass('bb-1');
 }
 
@@ -141,16 +141,34 @@ addlist.addEventListener('click', function(e){
 });
 
 //刪除待辦事項
+list.addEventListener('click', function(e){
+  if(e.target.getAttribute('class') !== 'del'){
+    return;
+  }
+  categoryLiBottom();
+  let num = e.target.getAttribute('data-num');
+  data.splice(num, 1);
+  renderData();
+  toBeCompletedNum();
+});
 
-
-
+// 完成待辦事項
+list.addEventListener('click', function(e){
+  if(e.target.getAttribute('class') !== 'checkbox' && e.target.getAttribute('class') !== 'material-icons'){
+    return;
+  }
+  categoryLiBottom();
+  data[e.target.getAttribute('data-num')].done =!data[e.target.getAttribute('data-num')].done;
+  renderData();
+  toBeCompletedNum();
+});
 
 
 
 
 //待辦事項總數
-function toBeCompleteNum(){
-  let toBeCompletedNum = 0;
+function toBeCompletedNum(){
+  let toBeCompleteNum = 0;
   data.forEach(function(item, index){
     if(item.done == false){
       toBeCompleteNum += 1;
@@ -159,10 +177,37 @@ function toBeCompleteNum(){
     const summary = document.querySelector('.summary');
     let newSummaryHTML = '';
     newSummaryHTML = `
-    <li class="todoitems">${toBeCompletedNum}個待完成項目</li>
+    <li class="todoitems">${toBeCompleteNum}個待完成項目</li>
       <li class="clear">清除已完成項目</li>
     `
     summary.innerHTML = newSummaryHTML;
   });
 };
 
+
+const clear = document.querySelector('.clear');
+const summary = document.querySelector('.summary');
+
+summary.addEventListener('click', function(e){
+  // 變數 count 儲存 done == true 的數量
+  let count = 0;
+
+  if(e.target.getAttribute('class') !== 'clear'){
+    return;
+  }
+
+  // 篩選物件內 done == true 的項目數量
+  data.filter(function(item, num){
+    if(item.done == true){
+      count = num;
+    }
+  });
+
+  data.forEach(function(item, index){
+    if(item.done == true){
+      data.splice(index, count);
+      // 刪除物件內 done == true 的項目數量
+      renderData();
+    }
+  });
+});
